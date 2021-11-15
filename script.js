83041d9c8f8d78b9907c64f9414e6ac4fb00e1d1
Simon Kluchko <simon@Users-MacBook-Pro-2.local>
2021-11-15 19:51:53 +0200

    fix slow scrolling and overlapping navbar in mobile version

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const d = document,
     modalLinks = d.querySelectorAll('.menu-item'),
     theHomeLink = navbar.firstChild;
 
+window.HTMLElement.prototype.scrollIntoView = function () { };
 
 // open dropdown list when user clicks on burger button
 burgerBtn.addEventListener('click', () => {
@@ -25,6 +26,8 @@ closeBtn.addEventListener('click', () => {
     closeBtn.classList.remove('close-btn_hidden');
 })
 
+
+
 // click event on each anchor link should close the dropdown, first anchor will reload the page
 modalLinks.forEach(item => {
     item.addEventListener('click', (event) => {
@@ -32,8 +35,11 @@ modalLinks.forEach(item => {
         burgerBtn.classList.remove('burger-btn_hidden');
         navbar.classList.remove('menu-flex');
         closeBtn.classList.remove('close-btn_hidden');
-
     })
 })
+
+
+
+
 
 
